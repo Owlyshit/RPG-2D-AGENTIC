d844ea8296d4f0b828b40c402c20dcdf38cee664
Owlyshit <liad.wolf@Mail.huji.ac.il>
2026-07-22 16:02:09 +0300

    feat: add melee equipment and boss boundaries (#20)

--- src/item.py
+++ src/item.py
@@ -13,3 +13,13 @@ class ManaPotion(Item):
     def __init__(self):
         super().__init__("mana_potion", "Mana Potion")
         self.restore_mp_amount = 20
+
+class Weapon(Item):
+    def __init__(self, item_id, name, attack_bonus, attack_range):
+        super().__init__(item_id, name, stack_limit=1)
+        self.attack_bonus = attack_bonus
+        self.attack_range = attack_range
+
+class BronzeSword(Weapon):
+    def __init__(self):
+        super().__init__("bronze_sword", "Bronze Sword", attack_bonus=8, attack_range=42)
